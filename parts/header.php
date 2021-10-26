d2f73eebf2098f984dd9e38bd0b6cb0759f7cc99
Voda Daniel <you@example.com>
2021-10-26 15:33:10 +0300

    updated header

--- parts/header.php
+++ parts/header.php
@@ -13,67 +13,80 @@
     <title>Blogu' National02</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid bg-light">
         <div class="row">
-            <div class="col-12">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="index.php">
-                        <img src="images/Black H Logo.png" alt="" width="80" height="24">
-                    </a>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php">Home</a>
-                            </li>                            
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Categorii
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <?php $categories = Category::findAll(); ?>
-                                    <?php foreach ($categories as $categoryObj): ?>
-                                        <a class="dropdown-item" href="category.php?id=<?php echo $categoryObj->getId(); ?>"><?php echo $categoryObj->name ?></a>
-                                    <?php endforeach; ?>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="contact.php">Contact</a>
-                            </li>
-                            <li class="nav-item">
-                                <?php if (getAuthUser()): ?>
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-6">
-                                                Salut, <b><?php echo getAuthUser()->getUsername(); ?></b>
-                                            </div>
-
-                                            <div class="col-6">
-                                                <a href="processLogout.php">Logout</a>
-                                            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <a class="navbar-brand" href="index.php">
+                                <img src="images/Black H Logo.png" alt="" width="80" height="24">
+                            </a>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="index.php">Home</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Categorii
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <?php $categories = Category::findAll(); ?>
+                                            <?php foreach ($categories as $categoryObj): ?>
+                                                <a class="dropdown-item" href="category.php?id=<?php echo $categoryObj->getId(); ?>"><?php echo $categoryObj->name ?></a>
+                                            <?php endforeach; ?>
                                         </div>
-                                    </div>
-                                <?php else: ?>
-                                    <a href="login.php">Login</a>
-                                    <a href="newUser.php">
-                                        <span>Cont nou</span>
-                                    </a>
-                                <?php endif; ?>
-                            </li>
-                            <?php if (getAuthUser() && getAuthAdmin()): ?>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="../blog/adminPage.php">Admin Panel</a>
-                                </li>
-                            <?php endif; ?>
-                        </ul>
-                        <form class="d-flex" method="get" action="search.php">
-                            <input class="form-control me-2" type="search" placeholder="Cauta" aria-label="search" name="search">
-                            <button class="btn btn-outline-success" type="submit">Cauta</button>
-                        </form>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="contact.php">Contact</a>
+                                    </li>
+                                </ul>
+                                <form class="d-flex mt-2" method="get" action="search.php">
+                                    <input class="form-control me-2" type="search" placeholder="Cauta" aria-label="search" name="search">
+                                    <button class="btn btn-outline-success" type="submit">Cauta</button>
+                                </form>
+                            </div>
+                        </nav>
                     </div>
-                </nav>
+                </div>
+            </div>
+            <div>
+               <div class="container">
+                   <div class="row">
+                       <div class="col-12 mt-4">
+                           <ul class="navbar-nav">
+                               <li class="nav-item">
+                                   <?php if (getAuthUser()): ?>
+                                       <div class="container">
+                                           <div class="row">
+                                               <div class="col-6">
+                                                   Salut, <b><?php echo getAuthUser()->getUsername(); ?></b>
+                                               </div>
+
+                                               <div class="col-6">
+                                                   <a href="processLogout.php">Logout</a>
+                                               </div>
+                                           </div>
+                                       </div>
+                                   <?php else: ?>
+                                       <a href="login.php" class="text-succes">Login</a>
+                                       <a href="newUser.php">
+                                           <span>Cont nou</span>
+                                       </a>
+                                   <?php endif; ?>
+                               </li>
+                               <?php if (getAuthUser() && getAuthAdmin()): ?>
+                                   <li class="nav-item">
+                                       <a class="nav-link" href="../blog/adminPage.php">Admin Panel</a>
+                                   </li>
+                               <?php endif; ?>
+                           </ul>
+                       </div>
+                   </div>
+               </div>
             </div>
         </div>
     </div>
-
 </body>
 </html>
