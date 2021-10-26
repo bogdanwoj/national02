f648138ea403dd0c3fd8a880c06725b0f3bf841e
Voda Daniel <you@example.com>
2021-10-26 18:46:53 +0300

    updated index.php

--- index.php
+++ index.php
@@ -1,22 +1,42 @@
 <?php
 include "parts/header.php";
 ?>
-<div class="container">.'
+<div class="container">
         <div class="row">
             <div class="col-12 mt-5 mb-2">
                  <h2>Cele mai noi stiri</h2>
             </div>
         </div>
     </div>
-    <div class="container">
+    <div class="container d-flex justify-content-between">
+    <div class="justify-content-start col-8">
+
 <?php
     $newArticleIds = query('SELECT id FROM articles ORDER BY id DESC LIMIT 10;');
     foreach ($newArticleIds as $newArticleId){
         $article = new Article($newArticleId['id']);
         $article->card();
 }
-?>.'
-	   
+?>
+    </div>
+       <div class="justify-content-end col-4" style="height: 300px">
+          <h4>
+              Curiozitati
+          </h4>
+          <?php
+          $article->cardCarousel();
+          ?>
+           <div class="mt-5">
+               <h4>
+                   Despre National02
+               </h4>
+               <p>
+                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non vestibulum eros, eget pretium massa.
+                   Aliquam molestie mi ac arcu consequat fermentum.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                   Phasellus nec pretium felis. Phasellus rutrum scelerisque eros non rutrum. Fusce sit amet turpis augue <a href="contact.php">aici.</a>
+               </p>
+           </div>
+       </div>
     </div>
 
 <?php
