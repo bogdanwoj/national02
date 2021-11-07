40774d054b107d8558a624d77abf01fdc47e77b0
bogdanwoj <bogdanwojcicki31@gmail.com>
2021-11-07 16:42:37 +0200

    integrate twig in insertCategory

--- insertCategory.php
+++ insertCategory.php
@@ -1,15 +1,5 @@
-<?php include "parts/header.php"; ?>
+<?php include "parts/header.php";
+$user = getAuthUser();
 
-    <div class="container mt-5">
-        <div class="row">
-            <h2> Adauga categorie noua </h2>
-            <div class="col-12 mt-5">
-                <form method="post" action="processInsertNewCategory.php" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="name">Nume categorie:</label>
-                        <input type="text" name="name" class="form-control" id="name" placeholder="Adauga categorie">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Adauga categorie</button>
-                </form>
-            </div>
-    </div>
+$template = $twig->load('insertCategory.html.twig');
+echo $template->render(['user'=>$user]);
