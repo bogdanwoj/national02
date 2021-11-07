0ca60254315ada36faf7732fcc327f34c2d92f87
bogdanwoj <bogdanwojcicki31@gmail.com>
2021-11-07 15:33:10 +0200

    twig updated merch page

--- classes/Product.php
+++ classes/Product.php
@@ -49,21 +49,4 @@ class Product extends Base
         return $this->name;
     }
 
-
-    public function productCard()
-    {
-        $productHTML= '<div class="col-3 ">
-            <div class="card text-center px-2">
-                <a href="product.php?id='.$this->getId().'"> <img class="card-img-top" src="images/'.$this->getFirstImage()->file.'" alt="'.$this->name.'" style="height: 260px;"></a>
-                <div class="card-body">
-                    <h5 class="card-title">'. $this->getName().'</h5> 
-                    <h5>'.$this->getPrice().'Lei
-                    </h5>
-                    ';
-        $productHTML.='<a href="addToCart.php?id='.$this->getId().'" class="btn btn-primary mt-2">Adauga in cos</a>';
-        $productHTML.='</div>
-            </div>
-        </div>';
-        echo $productHTML;
-    }
 }
--- product.php
+++ product.php
@@ -1,19 +1,11 @@
 <?php
-include "parts/header.php"
-?>
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <h2>Cumpara tricou personalizat</h2>
-        </div>
-        <div class="col-12">
-            <?php
-            $newProductIds = query('SELECT id FROM products ORDER BY id DESC LIMIT 4;');
-            foreach ($newProductIds as $newProductId){
-                $product = new Product($newProductId['id']);
-                $product->productCard();
-            }
-            ?>
-        </div>
-    </div>
-</div>
+include "parts/header.php";
+
+$newProductIds = query('SELECT id FROM products ORDER BY id DESC LIMIT 4;');
+$products = [];
+foreach ($newProductIds as $newProductId) {
+    $product = new Product($newProductId['id']);
+
+}
+$template = $twig->load('product.html.twig');
+echo $template->render(['product'=>$products]);
