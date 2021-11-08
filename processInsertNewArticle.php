f879271c993c64c5b81e9a90ad189dd46711d7e3
Alin <alin.iancu95@gmail.com>
2021-11-08 16:29:28 +0200

    integrate twig in insertNewArticle

--- processInsertNewArticle.php
+++ processInsertNewArticle.php
@@ -2,11 +2,13 @@
 include "functions.php";
 $files = $_FILES;
 
-copy($files['image']['tmp_name'],'/var/www/html/national02/bogdanw/blog/images/'.$files['image']['name']);
+copy($files['image']['tmp_name'],'/var/www/html/national02/alin/national02/images/'.$files['image']['name']);
 
 $articleId = new Article();
 $articleId->fromArray($_POST);
-
+if (getAuthUser()){
+    $articleId->userId = getAuthUser()->getId();
+}
 $articleId->save();
 
 
